6d5372281c0a23dd8b72c152cb10d0103ea25ac3
Dutervil <dwadson3@gmail.com>
2022-04-26 14:33:18 -0700

    ok

--- Client.java
+++ Client.java
@@ -132,8 +132,7 @@ public static Client  saisirCLient(){
              System.out.println("       D) GESTION CLIENTS");
              System.out.println("       E) RAPPORT VOITURES");
              System.out.println("       F) AIDE");
-             System.out.println("       F) AIDE");
-             System.out.println("       F) AIDE");
+             
 
              Scanner sc=new Scanner(System.in);
 
